de6a26a4012699fc0802726777d736d289ee33e3
EmnetWG <75603500+EmnetWG@users.noreply.github.com>
2023-05-30 10:01:10 -0400

    Update signup.php

--- Tourist/signup.php
+++ Tourist/signup.php
@@ -1,64 +1,38 @@
 <?php
+include("functions.php");
 $conn=mysqli_connect("localhost:3307", "root", "");
 mysqli_select_db($conn, "tourist");
-$passErr = $emailErr =$addressErr=$cityErr=$lastErr=$firstErr=$confErr=" ";
 
+$passErr = $emailErr =$addressErr=$cityErr=$lastErr=$firstErr=$confErr=$phoneErr=" ";
 if(isset($_POST['submit'])) {
 		
 
-if(empty($_POST['email'])){
-	 $emailErr = "email is required";	
-	}
-	else{
-		$email=$_POST['email'];
-	}
+if((!empty($_POST['email']))&&(!empty($_POST['firstname']))&&(!empty($_POST['lastname']))
+&&(!empty($_POST['phone']))&&(!empty($_POST['city']))&&(!empty($_POST['password']))&&
+(!empty($_POST['confirmpass']))){
+	 
 	
-
-	if(empty($_POST['firstname'])){
-	 $firstErr = "First name is required";	
-	}
-	else{
-		$first=$_POST['firstname'];
-	}
+    $address=$_POST['address'];
+   
 	
-	if(empty($_POST['lastname'])){
-	 $lastErr = "Last name is required";	
-	}
-	else{
-		$last=$_POST['lastname'];
-	}
-	
-	if(empty($_POST['city'])){
-	 $cityErr = "City is required";	
-	}
-	else{
-		$city=$_POST['city'];
-	}
-	
-	if(empty($_POST['password'])){
-	 $passErr = "Password is required";	
-	}
-	else{
-		$pass=$_POST['password'];
-	}
-	
-	if(empty($_POST['confirmpass'])){ 
-	 $confpassErr = "Password is required";	
-	}
-	else{
-		$confpass=$_POST['confirmpass']; 
-	}
-	
-	$address=$_POST['address'];
+    $email=$_POST['email'];
+    $first=$_POST['firstname'];
+    $last=$_POST['lastname'];
+    $phone=$_POST['phone'];
+    $pass=$_POST['password'];
+    $city=$_POST['city'];
+    $confpass=$_POST['confirmpass']; 
 	
 	 $email=stripslashes($email);
 	 $first=stripslashes($first);
 	 $last=stripslashes($last);
+   $phone=stripslashes($phone);
 	 $city=stripslashes($city);
 	 $address=stripslashes($address);
 	 $pass=stripslashes($pass);
 	 $confpass=stripslashes($confpass);
 	 $email=mysqli_real_escape_string($conn, $email);
+   $phone=mysqli_real_escape_string($conn, $phone);
 	 $first=mysqli_real_escape_string($conn, $first);
 	 $last=mysqli_real_escape_string($conn, $last);
 	 $pass=mysqli_real_escape_string($conn, $pass);
@@ -67,19 +41,20 @@ if(empty($_POST['email'])){
 $confpass=mysqli_real_escape_string($conn, $confpass);
 $hashpassword=password_hash($pass, PASSWORD_DEFAULT);
 	 
-	 $query="select * from user where email='$email'";
+	 $query="select * from `user` where email='$email'";
 $querys=mysqli_query($conn, $query);
 $row_count=mysqli_num_rows($querys);
 if($row_count>0){
-	$emailError="Email is already present";
+	$emailErr="Email is already present";
 }
+
 elseif($pass!=$confpass){
 	$confErr="Password does not much";
 }
 else  
 {
-$queryi="insert into `user` (`firstname`, `lastname`, `city`, `address`, `email`,`password`, `type`)
-	values( '$first', '$last', '$city', '$address', '$email', '$hashpassword', 'user')";
+$queryi="insert into `user` (`firstname`, `lastname`, `phone`, `city`, `address`, `email`,`password`, `type`)
+	values( '$first', '$last', '$phone', '$city', '$address', '$email', '$hashpassword', 'user')";
 	$querysi=mysqli_query($conn, $queryi);
 	if($querysi){
 
@@ -88,19 +63,34 @@ mysqli_close($conn);
 	
 }	
 }
-
-
-
-
-
+else{
+  $passErr = $emailErr =$addressErr=$cityErr=$lastErr=$firstErr=$confErr=$phoneErr="It is required ";
+}
+}
 ?>
-<html>
-<head></head>
-<title>Sign up</title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <title>Sign Up</title>
+    <style>
+    
+        
+        
+        </style>
+</head>
 <body>
-<div>
-
-<table>
+<?php
+include("header.php");
+?>
+    <main>
+<section class="padding-block-900">
+<div class="container " >
+<div  class="form-details">
+<table >
 <tr><td>
 <form action=" " method="POST" enctype="multipart/form-data" autocomplete="off">
 <table cellspacing=15>
@@ -113,6 +103,11 @@ mysqli_close($conn);
 <td><label>Last Name</label></td>
 <td>
 <input type="text" name="lastname" ><span class="error">* <?php echo $lastErr;?></span>
+<br><br></td></tr>
+<tr>
+<td><label>Phone Number</label></td>
+<td>
+<input type="text" name="phone" ><span class="error">* <?php echo $phoneErr;?></span>
 <br><br></td></tr>
 <tr>
 <td><label>City</label></td>
@@ -140,8 +135,8 @@ mysqli_close($conn);
 <input type="password" name="confirmpass"><span class="error">* <?php echo $confErr;?></span>
 <br><br></td></tr>
 <tr>
-<td><a href="login.php">Login</a></td>
-<td><input type="submit" name="submit" value="Signup"></td>
+<td><a href="login.php" style="color:var(--clr-primary-400);">Login</a></td>
+<td><button type="submit" name="submit"  class="btn">Sign up</button></td>
 </tr>
 </table>
 </form>
@@ -149,5 +144,15 @@ mysqli_close($conn);
 </tr>
 </table>
 </div>
+</div>
+</section>
+    </main>
+    <?php
+include("footer.php");
+    ?>
+    
+   
+   <script src="/main.js"></script> 
 </body>
 </html>
+
