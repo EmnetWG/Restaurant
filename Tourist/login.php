888097f02d3d787502c342f6ca70e2bdcc7cb6de
EmnetWG <75603500+EmnetWG@users.noreply.github.com>
2023-05-30 09:52:22 -0400

    Update login.php

--- Tourist/login.php
+++ Tourist/login.php
@@ -20,7 +20,11 @@ if(empty($_POST['password'])){
 	else{
 		$pass=$_POST['password'];
 	}
-	
+	 
+  $email=$_POST['email'];
+  
+    $pass=$_POST['password'];
+
 	 $email=stripslashes($email);
 	 $pass=stripslashes($pass);
 	 $email=mysqli_real_escape_string($conn, $email);
@@ -29,10 +33,30 @@ if(empty($_POST['password'])){
 	 $query="select * from `user` where `email`='$email'";
      $querys=mysqli_query($conn, $query);
      $row_count=mysqli_num_rows($querys);
-	 $rowp=mysqli_fetch_array( $querys, MYSQLI_ASSOC);
-	  $username=$rowp['firstname'];
-      $password=$rowp['password'];
-	  
+	
+
+      if($row_count>0){
+        $rowp=mysqli_fetch_array( $querys, MYSQLI_ASSOC);
+        $username=$rowp['firstname'];
+          $password=$rowp['password'];
+         
+          $type=$rowp['type'];
+
+        if(password_verify($pass,$password )) {
+     $_SESSION['useremail']=$email;
+   $_SESSION['username']=$username;
+        
+      if($type=="admin"){
+       
+        echo "<script>window.open('admin.php', '_SELF')</script>";
+
+      }
+      
+       
+        
+      
+    
+      else {
 	  $ipt=getIPAddress(); 
 
 		
@@ -41,12 +65,7 @@ $queryc="select * from `cart` where `ipaddress`='$ipt'";
 $querysc=mysqli_query($conn, $queryc);
 $product_count=mysqli_num_rows($querysc);
 
-      if($row_count>0){
-		  
-
-      if(password_verify($pass,$password )) {
-	 $_SESSION['useremail']=$email;
- $_SESSION['username']=$username;
+     
         if($product_count==0) {
 	  header('location:front.php');
 	//mysqli_close($conn);
@@ -55,28 +74,59 @@ $product_count=mysqli_num_rows($querysc);
 			
 			echo "<script>window.open('payment.php', '_SELF')</script>";        
 		}
-	  }
+      }}
+
 	else {
 		echo 'Invalid email or password';
 		
 	}
 	
-}
+      }
 else {
 	echo 'Invalid credentials';
 }
 
-
 }
-	 
 
+	 
+      
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<title>Login</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <title>Log in</title>
+	<style>
+	@media  (min-width: 50em) {
+.form-details{
+    padding-left:30%;
+}
+        }
+        .form-details input{
+            width:95%;
+border-radius:4px;
+box-sizing:border-box;
+padding:10px;
+border: none;
+
+border:1px solid #eee;
+background-color:#f7f7f7;
+
+
+        } 
+        </style>
 </head>
 <body>
-<div>
+<?php
+include("header.php");
+?>
+    <main>
+	<section class="padding-block-900">
+    <div class="container">
+<div class="form-details">
 
 <table>
 <tr><td>
@@ -85,16 +135,16 @@ else {
 <tr>
 <td><label>Email</label></td>
 <td>
-<input type="email" name="email" ><span class="error">* <?php echo $emailErr;?></span>
+<input type="email" name="email" onfocus="this.value=''"><span class="error">* <?php echo $emailErr;?></span>
 <br><br></td></tr>
 <tr>
 <td><label>Password</label></td>
 <td>
-<input type="password" name="password"><span class="error">* <?php echo $passErr;?></span>
+<input type="password" name="password" onfocus="this.value=''"><span class="error">* <?php echo $passErr;?></span>
 <br><br></td></tr>
 <tr>
-<td><a href="signup.php">Sign up</a></td>
-<td><input type="submit" name="submit" value="Login"></td>
+<td><a href="signup.php" style="color:var(--clr-primary-400)">Sign up</a></td>
+<td><button type="submit" name="submit"  class="btn">Login</button></td>
 </tr>
 </table>
 </form>
@@ -102,7 +152,18 @@ else {
 </tr>
 </table>
 </div>
-
-
-	 </body>
+	</div>
+	</section>
+    </main>
+  <?php
+include("footer.php");
+    ?>
+    
+   
+   <script src="/main.js"></script> 
+</body>
 </html>
+
+
+
+	 
