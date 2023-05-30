b22ea196c8600c78635182fd25f0cfba62e8c4b8
EmnetWG <75603500+EmnetWG@users.noreply.github.com>
2023-05-30 09:45:39 -0400

    Add files via upload

---
+++ Tourist/footer.php
@@ -0,0 +1,35 @@
+<footer class="bg-accent-400 fs-200">
+       <div class="container padding-block-900">
+          <div class="even-columns" style="align-items: flex-start;">
+           <div class="spacing">
+             <h4 class="fs-300 text-primary-400 fw-semibold">Useful Links</h4>
+             <ul role="list">
+             <li><a href="about.php" >About Us</a></li>
+             <li><a href="contact.php" >Contact Us</a></li></ul>
+             
+           </div>
+           <div class="spacing">
+             <h4 class="fs-300  text-primary-400 fw-semibold">Opening</h4>
+             <ul role="list" class="footer-nav fs-body text-neutral-100 ">
+               <li>Monday - Saturday</a></li>
+               <li>09AM-09PM</li>
+               <li>Sunday</li>
+               <li>10PM-08PM</li>
+               
+              </ul>
+           </div>
+           <div class="contact-address spacing">
+             <h4 class="fs-300 text-primary-400 fw-semibold">Contact</h4>
+             <ul role='list' class="text-neutral-100">
+             <li><img src="images/location.svg" /> 123 Avenue Green city</li>
+             <li><img src="images/phone.svg" /> +1234567</li>
+             <li><img src="images/mail.svg" /> info@saronmedical.com</li>
+              <a aria-label="facebook" href="#"></a>
+              <a aria-label="twitter" href="#"></a></li>
+            </ul>
+            </div>
+          </div>
+        </div>
+    </footer>
+    
+    
