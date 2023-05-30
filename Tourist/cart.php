a16fdacbb4e09997590da531c5b0defab25aeefa
EmnetWG <75603500+EmnetWG@users.noreply.github.com>
2023-05-30 09:26:21 -0400

    Update cart.php

--- Tourist/cart.php
+++ Tourist/cart.php
@@ -1,125 +1,93 @@
 <?php
-
-@session_start();
+session_start();
+include('functions.php');
 $conn=mysqli_connect("localhost:3307", "root", "");
 mysqli_select_db($conn, "tourist");
-
-include('functions.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-<title> Cart</title>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="style.css" />
+    
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"></script>
-<html>
-<head>
-
-<style>
-body{
-margin:0;
-padding:0;
-
-}
+    <title>Cart</title>
+    <style>
 
 
-
-
-.btn{
-color:orange;	
-}
- .btn a{
-color:orange;
-text-decoration:none;	
-}
-
-
-
-a, .nav-link {
-
-color:orange;
-text-decoration:none;
-}
-
-.tableContainer{
-	width:50%;
-	margin:auto;
-	margin-top:5rem;
-}
 th{
 	
 	background-color:#f2f2f2;
-	padding:10px;
+	padding:1rem;
 	
 }
-td{
-padding:10px;	
+
+    
+tr{
+  border-radius: 3px;
+    padding: 15px 20px;
+  box-shadow: 0px 0px 9px 0px rgba(0,0,0,0.1);
+  margin-bottom: 1.5rem;
 }
+
+
 .inputQty{
 	width:50px;
 }
+.shopping button{
+    margin-right:2rem;
+    margin-bottom: 2rem;
+}
+
+@media all and (max-width: 767px) {
+    .shopping{
+        padding-left: 2rem;
+    }
+    .cart-details{
+        margin: auto;
+      }
+    th {
+      display: none;
+    }
+    
+    tr {
+      display: block;
+    }
+    td {
+      
+      display: flex;
+      flex-basis: 100%;
+      padding: 10px 0;
+    }
+      td:before {
+        color: #6C7A89;
+        padding-right: 10px;
+        content: attr(data-label);
+        flex-basis: 50%;
+        text-align: right;
+      }
+      
+    }
+  
+
 </style>
+
 </head>
 <body>
-<header>
-<div >
-
-<!--<h2><?php cart_quan(); ?></h2>-->
-
-
-
-<nav class="navbar navbar-expand-md bg-dark  navbar-dark">
-
-
-<a class="navbar-brand" href="#"><img src="images/pay.jpg" height="20" class="rounded px-2"/></a>
-<button class="navbar-toggler" type="button" data-bs-toggle="collapse"  data-bs-target="#collapsibleNavbar">
-<span class="navbar-toggler-icon"></span></button>
-
-<div class="collapse navbar-collapse" id="collapsibleNavbar">
-<ul class="navbar-nav" >
-<li class="nav-item"><a  class="nav-link" href='#'>Home</a></li>
-<li class="nav-item"><a  class="nav-link" href='#'>About us</a></li>
-<?php if(isset($_SESSION['useremail'])) {
-echo '<li class="nav-item"><a class="nav-link" href="profile.php">My account</a></li>';
-
-}
-else
-{
-echo '<li class="nav-item"><a class="nav-link" href="signup.php.php">Register</a></li>';	
-}
-
-?>
-
-<li class="nav-item"><a class="nav-link" href='#'>About us</a></li>
-<li class="nav-item"><a class="nav-link" href='#'>Contact</a></li>
-</ul>
-</div>
-
-<span class="navbar-text" ><?php cart_quan() ?></span>
-<span class="navbar-text"><?php cart_price() ?></span>
-<?php if(isset($_SESSION['useremail'])){
-//echo'<span class="navbar-text">Welcome '.$_SESSION['username'].'</span>';
- 
-echo'<button type="button" class="btn btn-light" ><a href="logout.php">logout</a></button>';
-}
-else{
-echo'<button type="button" class="btn btn-light"><a href="login.php">login</a></button>';	
-}
-?>
-
-<button type="button" class="btn">search</button>
-
-</nav>
-</div>
-</header>
-<main>
-
-<div>
 <?php
+include("header.php");
+?>
+    <main>
+        <section class="padding-block-900">
+    
 
+
+
+<?php
+echo'<div class="container">';
 $product_list="";
 $ips=getIPAddress(); 
 
@@ -132,7 +100,7 @@ if($product_count>0)
 {
 	
 echo ' 
-<div class="tableContainer"><table ><tr><th>Image</th><th>Name</th><th>Description</th><th>Price</th><th>Remove</th><th>Quantity</th><th>Action</th></tr>';
+<table class="cart-details text-align-center"><tr><th>Image</th><th>Name</th><th>Description</th><th>Price</th><th>Remove</th><th>Quantity</th><th>Action</th></tr>';
 while($row=mysqli_fetch_array($querys, MYSQLI_ASSOC)){
 	
 $id=$row["idfood"];
@@ -151,16 +119,16 @@ $query_food="select * from food where idfood=$id";
  
 
             echo'
-			<tr><td><img src="'.$pros.'" width="50" height="50"></td> 
-            <td>'.$name.'</td>
-			<td>'.$description.'</td>
-            <td>'.$price.'</td>
+			<tr><td data-label="Image"><img src="'.$pros.'" width="50" height="50"></td> 
+            <td data-label="Name">'.$name.'</td>
+			<td data-label="Description">'.$description.'</td>
+            <td data-label="Price">'.$price.'</td>
 			
 			<form action=" " method="POST">
-			<td><input type="checkbox" name="removevalue[]" value='.$id.'></td>
+			<td data-label="Remove"><input type="checkbox" name="removevalue[]" value='.$id.'></td>
 			<input type="hidden" name="update_id" value='.$id.' />
-			<td><input class="inputQty" type="text" name="qty" value='.$quantity.' /></td>
-            <td><input type="submit" name="update_cart" value="update">';
+			<td data-label="Quantity"><input class="inputQty" type="text" name="qty" value='.$quantity.' /></td>
+            <td data-label="Action"><input type="submit" name="update_cart" value="update" class="btn btn-inverted">';
  
 			if (isset($_POST["update_cart"])){
 				
@@ -186,7 +154,7 @@ $query_food="select * from food where idfood=$id";
 		echo'		 
 	
                
-				<td><input type="submit" name="remove_cart" value="remove"></td></form></tr> ';
+				<td><input type="submit" name="remove_cart" value="remove" class="btn btn-inverted"></td></form></tr> ';
 				
 				
 			    echo $itemr=remove_cart_item();
@@ -194,29 +162,42 @@ $query_food="select * from food where idfood=$id";
 		}
 		
 
-echo '</table><h4>Total price:';
+echo '</table><h4 class="text-align-center">Total price: $';
 cart_price();
-echo'</h4>
+
+echo'</h4><div class="shopping padding-block-700" >
 <button type="button" class="btn" ><a href="cart.php?delete_all" onclick="Confirmation ()">Delete all</a></button>';
 
 echo '<script type="text/javascript">function Confirmation(){return confirm("Are you sure you want to delete all ?");} </script>';
+
 }
+
          
 else
 {
-	echo '<div>';
+	echo '<div style="margin-bottom:2rem;">';
 $product_list="You have no products listed in your cart yet";
-echo $product_list;	
-echo '</div>';
+echo'<p class="text-neutral-300">'; 
+echo $product_list;
+echo '</p></div><div class="shopping padding-block-700" >';
 }
+
+echo '<button type="button" class="btn"><a href="front.php">Continue Shopping</a></button>
+<button type="button" class="btn btn-inverted"><a href="checkout.php" style="color:var(--clr-primary-400);">Checkout</a></button>
+</div>
+</div>'
 
 ?>
 
-<button type="button" class="btn"><a href="front.php">Continue Shopping</a></button>
-<button type="button" class="btn"><a href="checkout.php">Checkout</a></button>
 
-</div>
-</div>
+        </section>
 </main>
+
+<?php
+include("footer.php");
+    ?>
+    
+   
+   <script src="/main.js"></script> 
 </body>
 </html>
