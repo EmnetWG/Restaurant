5df79d784c3e4e11e3801cb246dfa0c46290a6d6
EmnetWG <75603500+EmnetWG@users.noreply.github.com>
2023-06-03 01:23:42 -0400

    Update session.php

--- Tourist/session.php
+++ Tourist/session.php
@@ -1,15 +1,20 @@
 <?php
 
-session_start();
+@session_start();
 $conn=mysqli_connect("localhost:3307", "root", "");
 mysqli_select_db($conn, "tourist");
 $userm=$_SESSION['useremail'];
-$query6="select `email` from `user` where `email`='$userm'";
+$userl=$_SESSION['userlevel'];
+$query6="select * from `user` where `email`='$userm'";
 $query7=mysqli_query($conn, $query6);
 $result=mysqli_fetch_assoc($query7);
 $qu=$result['email'];
+$level=$result['type'];
 if(!isset($qu)){
 	mysqli_close($conn);
 	header('location:login.php');
+}
+elseif($userl=="user"){
+    header('location:front.php');
 }
 ?>
